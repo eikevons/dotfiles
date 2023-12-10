0ef363814fd1b9b8c9013f98f495dd741610493b
Eike von Seggern <eike@vonseggern.space>
2023-12-10 14:33:12 +0100

    [zsh] add unquoting percent signs in recently-used files

--- zsh_extensions/recently-used.zsh
+++ zsh_extensions/recently-used.zsh
@@ -4,10 +4,13 @@ which fzf &>/dev/null && [[ -e ~/.local/share/recently-used.xbel ]] && {
   __recently_used() {
     setopt localoptions pipefail no_aliases 2> /dev/null
     local item
-    sed -n -e 's#<bookmark href="file://\(/[^"]*\)" .*$#\1#p' ~/.local/share/recently-used.xbel \
-      | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse --bind=ctrl-z:ignore ${FZF_DEFAULT_OPTS-}" fzf | while read item; do
-      echo -n "${(q)item} "
-    done
+    # Use solution form sed "s@+@ @g;s@%@\\\\x@g" | xargs -0 printf "%b"'
+    # https://askubuntu.com/a/295312
+    < ~/.local/share/recently-used.xbel \
+      sed -n -e 's#<bookmark href="file://\(/[^"]*\)" .*$#\1#p' \
+      | sed "s@+@ @g;s@%@\\\\x@g" | xargs -0 printf "%b" \
+      | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --bind=ctrl-z:ignore ${FZF_DEFAULT_OPTS-}" fzf \
+      | while read item; do echo -n "${(q)item} "; done
     local ret=$?
     echo
     return $ret
